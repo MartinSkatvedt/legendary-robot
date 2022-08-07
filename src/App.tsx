81adce3f60341f4a7e822405405d035b40450f7c
Martinskatvedt <martinskatvedt@gmail.com>
2022-08-07 22:24:14 +0200

    add firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Heading } from "@chakra-ui/react";
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+const readData = async () => {
+  const querySnapshot = await getDocs(collection(db, "shopping-list-db"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+  });
+};
 
 function App() {
+  readData();
 
   return (
-   <Box w="100%" textAlign="center">
-    <Heading>React + Chakra UI</Heading>
-   </Box> 
-  )
+    <Box w="100%" textAlign="center">
+      <Heading>React + Chakra UI</Heading>
+    </Box>
+  );
 }
 
-export default App
+export default App;
